f26d8962021c0dae3cb105e6b8218017e6afc32d
marcoandresv <153276680+marcoandresv@users.noreply.github.com>
2024-04-19 13:38:29 +0200

    Initial commit

---
+++ src/main/java/org/example/PersonsList.java
@@ -0,0 +1,15 @@
+package org.example;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class PersonsList {
+    private List<Person> person;
+
+    public PersonsList(){
+        this.person = new ArrayList<>();
+    }
+    
+    //Work in progress
+
+}
